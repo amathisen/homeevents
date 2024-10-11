dcd01fb15e782c5cb8694f3cf926325d03527bcb
Aaron Mathisen <35702793+amathisen@users.noreply.github.com>
2024-10-10 22:21:49 -0700

    User display page updates
    List all events user has participated in, including results and associated objects

--- user.php
+++ user.php
@@ -6,11 +6,7 @@ require_once('class/user.php');
 $user_id = get_form_value('user_id');
 $this_user = new User($user_id);
 
-if(isset($this_user->name))
-    $page_title = "User " . $this_user->name;
-else
-    $page_title = "User ??";
-
+isset($this_user->name) ? $page_title = "User " . $this_user->name : $page_title = "User ??";
 require_once('header.php');
 
 if(!$this_user || !isset($this_user->id) || $this_user->id != $user_id) {
@@ -19,11 +15,32 @@ if(!$this_user || !isset($this_user->id) || $this_user->id != $user_id) {
     exit;
 }
 
-$events = $this_user->get_referring_results_by_link('event_users','event');
 echo "<center><h1>" . $this_user->name . "</h1></center>";
-echo "<table name='user_events_table'>";
+
+$events = $this_user->get_referring_results_by_link('event_users','event');
 foreach($events as $this_event) {
+    $loopcount = 0;
+    echo "<table name='user_events_table_" . $this_event->id . "' border='1'>";
     echo "<tr><td><a href = 'event.php?event_id=" . $this_event->id . "'>" . $this_event->title . "</a></td><td>" . $this_event->date . "</td></tr>";
+    $event_activities = $this_event->get_referring_results('event_activities');
+    foreach($event_activities as $this_activity) {
+        ($loopcount % 2) ? $bgcolor = "82CFDF" : $bgcolor = "FFFFFF";
+        $activity = $this_activity->get_associated_result('activity');
+        $result = $this_activity->get_referring_results('event_activities_results',array('user_id',$this_user->id));
+        if(isset($result[0]->result_value)) {
+            $result_value = $result[0]->result_value;
+            $results_object = $result[0]->get_referring_results_by_link('event_activities_results_objects','activity_object');
+            isset($results_object[0]->name) ? $results_object_name = $results_object[0]->name : $results_object_name = null;
+        } else {
+            $result_value = "--";
+            $results_object_name = null;
+        }
+        echo "<tr style='background-color: " . $bgcolor . ";'><td>" . $this_activity->name . "</td><td>" . $activity->name . "</td><td>" . $result_value . "</td></tr>";
+        if($results_object_name != null)
+            echo "<tr style='background-color: " . $bgcolor . ";'><td>&nbsp;</td><td>" . $results_object_name . "</td></tr>";
+        $loopcount++;
+    }
+    echo "</table><br /><br />";
 }
-echo "</table>";
+
 require_once('footer.php'); ?>
